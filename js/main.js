6c0564243de757f629fa07523a698e6cd821c3fc
root <root@localhost>
2024-08-01 02:25:38 -0300

    auto publish 1722489938387

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let btnSi = document.getElementById("btnSi");
 let btnNo = document.getElementById("btnNo");
-let dvText = document.getElementById("dvText");
+let h3opcion = document.getElementById("h3opcion");
 let imgMuñoz = document.getElementById("imgMuñoz");
 let numeroIntentos = 0;
 
@@ -11,27 +11,23 @@ btnNo.onclick = function(){
       imgMuñoz.src = "./imgs/Muñoz3.jpg";
       btnNo.innerText = "Estoy segura";
       btnNo.style.fontSize = "20px";
-      //btnNo.style.width = "150px";
-      //btnNo.style.height = "60px";
     break;
     case 1:
       imgMuñoz.src = "./imgs/Muñoz4.jpg";
       btnNo.innerText = "Me voy con ayala";
       btnNo.style.fontSize = "17px";
-      //btnNo.style.width = "200px";
     break;
     case 2:
       imgMuñoz.src = "./imgs/Muñoz5.jpg";
       imgMuñoz.style.objectPosition = "bottom";
       btnNo.innerText = "Tengo miedo";
-      //btnNo.style.width = "150px";
     break;
     default:
       imgMuñoz.src = "./imgs/MuñozMalo.jpg";
       btnNo.addEventListener("click", redireccion)
+      h3opcion.style.opacity = "100%";
       btnNo.innerText = "ZI";
-      //btnNo.style.width = "100px";
-      //btnNo.style.height = "50px";
+      btnNo.style.fontSize = "25px";
     break;
   }
  
